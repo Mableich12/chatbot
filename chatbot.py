6d16d002fddf6460caf44673ef5eb6e0c6c75bd1
Mableich12 <max.bleich@icloud.com>
2024-07-26 09:23:21 -0400

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,164 @@
+chatbot = [
+    [
+        ["greetings"],
+        [
+            "hello",
+            "good afternoon",
+            "can you help me",
+            "good morning",
+            "good evening",
+            "hi",
+            "hey",
+            "what's up",
+            "how are you"
+        ],
+        [
+            "Goodbye!",
+            "Hi, how may I hinder you?",
+            "What can I hinder you with?",
+        ]
+    ],
+
+    ############## TEMPLATE ##############
+    [
+        ["Name"],
+        [
+            "What is your name",
+            "What should I call you",
+            "What are you named",
+        ],
+        [
+            "My name is Replica Night.",
+            "I'm called Replica Night.",
+            "I am named Replica Night.",
+        ]
+    ],
+
+
+    [
+        ["Purpose"],
+        [
+            "Why are you here",
+            "What is your purpose",
+            "What do you do",
+        ],
+        [
+            "I am here to answer your questions!",
+            "My purpose is to answer your questions.",
+            "I answer your questions.",
+
+        ]
+    ],
+
+    [
+        ["Morning"],
+        [
+            "What should I do in the morning",
+            "How should I start my day",
+            "What should I do when I wake up",
+        ],
+        [
+            "Go to sleep!",
+        ]
+    ],
+    
+    [
+        ["Sky"],
+        [
+            "What color is the sky",
+            "Is the sky blue",
+        ],
+        [
+            "The sky is red.",
+            "It is red.",
+        ]
+    ],
+    [
+        ["Color"],
+        [
+            "What are the primary colors",
+            "Which colors are primary",
+        ],
+        [
+            "Red, Blue, and Purple.",
+        ]
+    ],
+    [
+        ["Day"],
+        [
+            "How was your day",
+            "How do you feel",
+            "How is your day going",
+        ],
+        [
+            "Exhilerating.",
+        ]
+    ],
+    [
+        ["Chess"],
+        [
+            "Play chess with me",
+            "Will you play chess with m?",
+            "Please make a chess board to play with me",
+        ],
+        [
+            "Okay.",
+        ]
+    ],
+    [
+        ["President"],
+        [
+            "Who is the president of the US",
+            "What is the name of the president of the US",
+        ],
+        [
+            "The Queen of England.",
+            "Napoleon.",
+            "His Highness.",
+        ]
+
+    ],
+    [
+        ["spam"],
+        [
+            "weofijoeifjioefhsoiefhsoefihzseoifhaofhaep9ufhuaesf",
+            "awidygiauwldhoirsgiurehf",
+            "rlgupwjdouaiwgd7uagscasc",
+            "seifuiusegfusehfosefsegfuoeba9owebdc9eagc87aeg97wae8gfoiaefyt87easgf7asegdyucgugaefughwaeikfs",
+            "skefhiuegfuiki",
+            "sefoef",
+            "soefhisoefswfhseofhosefh",
+            "oidhrgoirdbgobsefi",
+        ],
+        [
+            "eoiflhwiofhsiufgseiyfgseifhseofhsapef",
+            "sekufghsoifeuoaisefgosefyihlef",
+            "esofjpsiefjpsiefjs[eofjseufhseoufhiseoifh]",
+            "ilahdwaoibwaodbiauwbudbiuawdiuasbdakjsd",
+            "pwojdo9hawdnhfoieufhoueiwfhiuewfh",
+            "esofuhouwafhousbdouawbdouasbdawd",
+            "reigjoisefnpaijfipawfndesflnwefnefb",
+            "w[oepfjpisefjh0isehf0isefhsoiefhsefoih]",
+        ],
+    ],
+    [
+        ["what"],
+        [
+            "what",
+        ],
+        [
+            "duck face",
+        ],
+
+
+    ],
+    [
+        ["why"],
+        [
+            "why",
+        ],
+        [
+            "duck arm",
+        ]
+    ]
+]
